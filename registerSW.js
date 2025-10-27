6e0bd2bc2ed5f830001568c78d50cb1365edf00d
Daphne <daphne.issac@gmail.com>
2025-10-28 00:14:25 +0530

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/task-sparkle-react/sw.js', { scope: '/task-sparkle-react/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/task-spark-react/sw.js', { scope: '/task-spark-react/' })})}
